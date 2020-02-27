7d4b973a739cd2d3558f9dc0c1e35c72705a1c62
Juan Camilo Pulido <jc.pulido@uniandes.edu.co>
2020-02-27 08:35:01 -0500

    Primer commit

---
+++ lib/proyecto1.ex
@@ -0,0 +1,18 @@
+defmodule Proyecto1 do
+  @moduledoc """
+  Documentation for Proyecto1.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Proyecto1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/proyecto1_test.exs
@@ -0,0 +1,8 @@
+defmodule Proyecto1Test do
+  use ExUnit.Case
+  doctest Proyecto1
+
+  test "greets the world" do
+    assert Proyecto1.hello() == :world
+  end
+end
