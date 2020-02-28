c11cefacee861f1c1630e123b601147eda6f2be5
Juan Camilo Pulido <jc.pulido@uniandes.edu.co>
2020-02-28 07:48:12 -0500

    Cambios inutiles

--- lib/proyecto1.ex
+++ lib/proyecto1.ex
@@ -12,7 +12,23 @@ defmodule Proyecto1 do
       :world
 
   """
-  def hello do
-    :world
+  def increment(actual_call, winner_number, telefone) do
+    lock = Mutex.await(MyMutex, telefone)
+    new_actual = {actual_call + 1, telefone}
+    IO.puts(new_actual)
+    new_actual
+    Mutex.release(MyMutex, lock)
+  end
+
+
+  def call_center(winner_number, win) do
+    actual_call = {0, 0}
+
+    spawn(fn -> increment(actual_call, winner_number, 1) end)
+    spawn(fn -> increment(actual_call, winner_number, 2) end)
+    spawn(fn -> increment(actual_call, winner_number, 3) end)
+    spawn(fn -> increment(actual_call, winner_number, 4) end)
+
+    # No se como hacer las variables globales, toca preguntar mañana
   end
 end
